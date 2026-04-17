50f1badca161052c5a0d02e73ab36c9a17c42245
joshuarabulan <joshuamacatangayrabulan.com>
2026-04-17 11:26:10 +0800

    new

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
