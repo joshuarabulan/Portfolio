b3773efe591f244d558cd82906bebd338a55c1d2
joshuarabulan <joshuamacatangayrabulan.com>
2026-04-18 00:33:48 +0800

    new

--- db.php
+++ db.php
@@ -5,16 +5,10 @@ $password = 'arx3ZHVQMQnH0Exq';
 $database = 'portfolio';
 $port = 4000;
 
-// Create connection with SSL
-$conn = mysqli_init();
+$conn = new mysqli($host, $username, $password, $database, $port);
 
-// Set SSL options (no certificate required for TiDB Serverless)
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
-
-// Connect with SSL
-if (!mysqli_real_connect($conn, $host, $username, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
